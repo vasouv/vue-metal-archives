65fffd84dff102ee3465c404e23e7519fc91fef9
Vasilis Souvatzis <vasouv@gmail.com>
2024-08-09 18:40:50 +0300

    stuff

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,29 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { ref } from "vue";
+  const selectedBand = ref('');
+
+  const bands = [
+    {id: 184, name: 'Bathory'},
+    {id: 4, name: 'Iced Earth'},
+    {id: 150, name: 'Amon Amarth'}
+  ]
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+  <header class="container">
+    <h1>Vue Metal Archives API</h1>
   </header>
 
-  <main>
-    <TheWelcome />
+  <main class="container">
+    <label for="band-select">Select Band:</label>
+    <select id="band-select" v-model="selectedBand">
+      <option value="" disabled>- Select Band -</option>
+      <option value="184">Bathory</option>
+      <option value="4">Iced Earth</option>
+      <option value="150">Amon Amarth</option>
+    </select>
+
+    <span>Selected band ID is: {{ selectedBand }}</span>
+
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
